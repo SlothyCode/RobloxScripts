177977c63c72a4491c516a6f5daff0375d8226bb
SlothyCode <skeletalorigin@gmail.com>
2016-01-09 17:08:16 +0000

    Update DoubleJump.lua

--- DoubleJump.lua
+++ DoubleJump.lua
@@ -6,30 +6,24 @@ local isSecondJumping = false
 local JumpPower = 40
 
 UserInput.InputBegan:connect(function(inputObj)
-	if inputObj.UserInputState == Enum.UserInputState.Begin then
-		if inputObj.KeyCode == Enum.KeyCode.Space then
-			isJumping = true
-			local init = tick()
-			local secondJump = UserInput.InputBegan:connect(function(iObj2)
-				if inputObj.UserInputState == Enum.UserInputState.Begin then
-					if inputObj.KeyCode == Enum.KeyCode.Space then
-						if tick() - init <= 0.55 and not isSecondJumping then
-							isSecondJumping = true
-							for i, bodyPart in next, Char:GetChildren() do
-								if bodyPart:IsA("BasePart") then
-									print(bodyPart.Name, "     ", bodyPart:GetMass() * JumpPower)
-									bodyPart.Velocity = Vector3.new(0, JumpPower * bodyPart:GetMass(), 0)
-									wait()
-									bodyPart.Velocity = Vector3.new(0, 0, 0)
-								end
-							end
-							repeat wait() until not Char:WaitForChild("Humanoid").Jump
-							isSecondJumping = false
-							isJumping = false
-						end
+	if inputObj.UserInputState == Enum.UserInputState.Begin and inputObj.KeyCode == Enum.KeyCode.Space then
+		isJumping = true
+		local init = tick()
+		local secondJump = UserInput.InputBegan:connect(function(iObj2)
+			if inputObj.UserInputState == Enum.UserInputState.Begin and inputObj.KeyCode == Enum.KeyCode.Space and tick() - init <= 0.55 and not isSecondJumping then				
+				isSecondJumping = true
+				for i, bodyPart in next, Char:GetChildren() do
+					if bodyPart:IsA("BasePart") then
+						print(bodyPart.Name, "     ", bodyPart:GetMass() * JumpPower)
+						bodyPart.Velocity = Vector3.new(0, JumpPower * bodyPart:GetMass(), 0)
+						wait()
+						bodyPart.Velocity = Vector3.new(0, 0, 0)
 					end
 				end
-			end)
-		end
+				repeat wait() until not Char:WaitForChild("Humanoid").Jump
+				isSecondJumping = false
+				isJumping = false
+			end
+		end)
 	end
 end)
