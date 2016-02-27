38241a03203a369e38e55f30938d298b5237ae2e
FrigidSloth <skeletalorigin@gmail.com>
2016-02-27 16:18:43 +0000

    Update AwardPointsToPlayer.lua

--- AwardPointsToPlayer.lua
+++ AwardPointsToPlayer.lua
@@ -2,14 +2,36 @@ local DataStore = game:GetService("DataStoreService")
 local PointsStore = DataStore:GetOrderedDataStore("Points")
 
 local function GetPlayerByPattern(pattern)
+	print(pattern)
 	for _, v in pairs(game.Players:GetPlayers()) do
     		if v.Name:lower():find(pattern:lower()) then
+			print(v.Name)
       			return v
     		end
   	end
 end
 
+
+
+local function SaveData(key, value)
+	return PointsStore:UpdateAsync(key, function(oldValue)
+		local newValue = oldValue or (value~=0 and value)
+		return newValue
+	end)
+end
+
+local function GetData(key)
+	return PointsStore:GetAsync(key)
+end
+
 game.Players.PlayerAdded:connect(function(player)
+	local ls = Instance.new("IntValue", player)
+	ls.Name = "leaderstats"
+	
+	local poi = Instance.new("IntValue", ls)
+	poi.Value = GetData(player.UserId) or 0
+	poi.Name = Instance.new("Points")
+	
 	player.Chatted:connect(function(msg)
     		if msg:sub(1, 6) == ":award" then
     			
@@ -31,8 +53,8 @@ game.Players.PlayerAdded:connect(function(player)
 			end
 			
 			plrPoints = plrPoints + numPoints
-			
+			SaveData(player.UserId, plrPoints)
 		end
-	    
 	 end)
+	
 end)
